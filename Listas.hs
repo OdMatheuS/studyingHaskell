f155058729df26b12949c7607cd9204905c7f964
Matheus Nagot <matheusnagot@hotmail.com.br>
2021-03-14 22:25:18 -0300

    feat: fn() multiplicar todos valores de uma lista

--- Listas.hs
+++ Listas.hs
@@ -2,3 +2,6 @@ module Listas where
 
 concatListasSingleValue :: [Int] -> [Int] -> [Int]
 concatListasSingleValue [a] [b] = [a,b]
+
+multiplicarValoresLista :: [Int] -> [Int]
+multiplicarValoresLista ab = [2*a | a<-ab]
