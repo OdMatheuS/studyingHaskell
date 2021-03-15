a2a08af62c206c121516eeea9b9f5afa62016d12
Matheus Nagot <matheusnagot@hotmail.com.br>
2021-03-14 23:00:29 -0300

    feat:fn() multiplicar valores lista com excecao

--- Listas.hs
+++ Listas.hs
@@ -5,3 +5,6 @@ concatListasSingleValue [a] [b] = [a,b]
 
 multiplicarValoresLista :: [Int] -> [Int]
 multiplicarValoresLista ab = [2*a | a<-ab]
+
+multiplicarComExcecao :: [Int]
+multiplicarComExcecao = [2*x+2 | x<-[0..8],x/=4]
